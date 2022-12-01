0e10e26ab9f5b5e983ec4f4921356bc48758d17e
a.privalov <a.privalov@jump.finance>
2022-12-01 11:00:19 +0300

    Изменения по негативному тесту

--- google_test.py
+++ google_test.py
@@ -6,6 +6,6 @@ def test_google_selene(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter() #Вводим нужное значение в поиск и жмем ввод
     browser.element('[id="search"]').should(have.text('Selene - Wikipedia')) #Проверяем что на странице присутствует 'Selene - Wikipedia'
 
-def test_google_cats(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('cats').press_enter() #Вводим нужное значение в поиск и жмем ввод
-    browser.element('[id="search"]').should(have.no.text('dogs')) #Проверяем что на странице НЕ присутствует 'dogs'
+def test_google_fake(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('fsgdassavcgtaewrcxasd').press_enter() #Вводим нужное значение в поиск и жмем ввод
+    browser.element('[id="search"]').should(have.text('нет полезных результатов')) #Проверяем что на странице нет результатов
